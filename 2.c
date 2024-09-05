1c5e35856c01b10a513b985e18737877272bcd0d
KOMAL4214 <jksai2006@gmail.com>
2024-09-05 17:42:18 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int c,f;
+    printf("program to convert temp from farenheit to celsius");
+    printf("\nenter the temp in farenheit");
+    scanf("%d",&f);
+    c=(5*(f-32))/9;
+    printf("tem in celsius=%d",c);
+
+}
